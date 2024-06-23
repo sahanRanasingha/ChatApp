870169d41ff9e458badf8f03cb46d1f61bfd2c7e
Sahan Ranasingha <sahanranasingha1@gmail.com>
2024-06-23 17:57:37 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -10,6 +11,8 @@ import { app, server } from "../backend/socket/soket.js";
 
 const PORT = process.env.PORt || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // To parse the incoming request with JSON payloads (from req.body)
@@ -18,6 +21,12 @@ app.use(cookieParser()); // To parse the incoming request cookies
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname,"frontend/dist")));
+
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
 
 server.listen(PORT,() => {
     connectToMongoDB();
